8aa164bc9e107b20e77d6d80bd9938c2f12fba80
steff-hub-max <sdahlberg858@gmail.com>
2025-09-30 13:51:15 +0200

    added Account.csv and made the program read and write to it.
    so that when you make an account, it will be saved. that way, the
    account can be used next time you start the program.

--- Program.cs
+++ Program.cs
@@ -1,26 +1,30 @@
 ﻿using App;
-
+string[] users_csv = File.ReadAllLines("Account.csv");
 List<Iuser> users = new List<Iuser>();
 List<Items> item = new List<Items>();
-users.Add(new Account("test", "a@a", "test"));
 Iuser? active_user = null;
+foreach (string user_data in users_csv)
+{
+  string[] split_user_data = user_data.Split(",");
+  users.Add(new Account(split_user_data[0], split_user_data[1], split_user_data[2]));
+}
 
 bool running = true;
 while (running)
 {
   if (active_user is Account a)
   {
-    Console.WriteLine("Welcome " + a.Name);
-    Console.WriteLine("\n\nnew- add an item to your inventory\nquit");
+    Console.WriteLine($"Welcome {a.Name}");
+    Console.WriteLine("\n\nnew- add an item to your inventory\nbrowse\nlogout");
     switch (Console.ReadLine())
     {
       case "new":
         Console.Write("Enter the name of the item: ");
         string? item_name = Console.ReadLine();
-        Console.Write("Enter a description of the item");
+        Console.Write("Enter a description of the item: ");
         string? item_info = Console.ReadLine();
         string item_owner = a.Name;
-        item.Add(new Items(item_name, item_info, item_owner));
+        item.Add(new Items(item_name!, item_info!, item_owner));
         break;
       case "browse":
         Console.WriteLine("All items available to trade: ");
@@ -33,7 +37,8 @@ while (running)
         }
         break;
 
-      case "quit":
+
+      case "logout":
         active_user = null;
         break;
     }
@@ -49,11 +54,11 @@ while (running)
         string? name = Console.ReadLine();
         Console.Write("Email: ");
         string? email = Console.ReadLine();
-        Console.Write("Password");
+        Console.Write("Password: ");
         string? _password = Console.ReadLine();
         users.Add(new Account(name, email, _password));
-
-
+        string[] new_account = { $"{name},{email},{_password}" };
+        File.AppendAllLines("Account.csv", new_account);
         break;
       case "login":
         if (active_user == null)
